561bb8e992022b613b314911a1dec5157e812e17
lavareas <63117847+lavareas@users.noreply.github.com>
2024-10-27 09:24:13 -0500

    Update AccountHolder.cs
    Adding JsonProperty tags for easier Json Parsing
    
    Signed-off-by: lavareas <63117847+lavareas@users.noreply.github.com>

--- src/FdxSharp/Models/AccountHolder.cs
+++ src/FdxSharp/Models/AccountHolder.cs
@@ -8,26 +8,31 @@
 		/// <summary>
 		/// Gets or sets the first name of the account holder.
 		/// </summary>
+		[JsonProperty("firstName")]
 		public string FirstName { get; set; }
 
 		/// <summary>
 		/// Gets or sets the last name of the account holder.
 		/// </summary>
+		[JsonProperty("lastName")]
 		public string LastName { get; set; }
 
 		/// <summary>
 		/// Gets or sets the address of the account holder.
 		/// </summary>
+		[JsonProperty("address")]
 		public string Address { get; set; }
 
 		/// <summary>
 		/// Gets or sets the phone number of the account holder.
 		/// </summary>
+		[JsonProperty("phoneNumer")]
 		public string PhoneNumber { get; set; }
 
 		/// <summary>
 		/// Gets or sets the email address of the account holder.
 		/// </summary>
+		[JsonProperty("email")]
 		public string Email { get; set; }
 	}
 }
