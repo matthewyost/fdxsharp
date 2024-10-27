b7c400aea81e86531c737e841b850fd2467d1e55
lavareas <63117847+lavareas@users.noreply.github.com>
2024-10-27 09:27:19 -0500

    Update Address.cs
    Adding JsonProperty Tags and Summaries for easy of JSON parsing and readability
    
    Signed-off-by: lavareas <63117847+lavareas@users.noreply.github.com>

--- src/FdxSharp/Models/Address.cs
+++ src/FdxSharp/Models/Address.cs
@@ -4,20 +4,52 @@ namespace FdxSharp.Models
 {
 	public class Address
 	{
+		/// <summary>
+		/// Address Line 1
+		/// </summary>
+		[JsonProperty("line1")]
 		public string? Line1 { get; set; }
 
+		/// <summary>
+		/// Address Line 2
+		/// </summary>
+		[JsonProperty("line2")]
 		public string? Line2 { get; set; }
 
+		/// <summary>
+		/// Address Line 3
+		/// </summary>
+		[JsonProperty("line3")]
 		public string? Line3 { get; set; }
 
+		/// <summary>
+		/// City
+		/// </summary>
+		[JsonProperty("city")]
 		public string? City { get; set; }
 
+		/// <summary>
+		/// State
+		/// </summary>
+		[JsonProperty("state")]
 		public string? State { get; set; }
 
+		/// <summary>
+		/// Region
+		/// </summary>
+		[JsonProperty("region")]
 		public string? Region { get; set; }
 
+		/// <summary>
+		/// Postal Code
+		/// </summary>
+		[JsonProperty("postalCode")]
 		public string? PostalCode { get; set; }
 
+		/// <summary>
+		/// Country
+		/// </summary>
+		[JsonProperty("country")]
 		public CountryCodeType Country { get; set; }
 	}
 }
